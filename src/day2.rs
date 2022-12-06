b45f310b3617b2cb20eb2dedf424fc32c7191099
Joseph Martinsen <josmarti@adobe.com>
2022-12-06 09:58:16 -0800

    simple cleanups d2

--- src/day2.rs
+++ src/day2.rs
@@ -1,101 +1,83 @@
 // ROCK  A <-> X: 1pt
 // PAPER B <-> Y : 2pt
 // SCISSORS  C <-> Z : 3pt
-
-pub struct Round(String, String);
+pub struct Round(char, char);
 
 #[aoc_generator(day2)]
 pub fn input_generator(input: &str) -> Vec<Round> {
-    let mut vals = Vec::new();
+    input
+        .lines()
+        .map(|l| Round(l.chars().nth(0).unwrap(), l.chars().nth(2).unwrap()))
+        .collect()
+}
 
-    for l in input.lines() {
-        let v: Vec<&str> = l.split(" ").collect();
-        vals.push(Round(v[0].to_string(), v[1].to_string()));
+fn get_winning_val(a: &char) -> &char {
+    match a {
+        'A' => &'B',
+        'B' => &'C',
+        'C' => &'A',
+        _ => unreachable!(),
     }
-    return vals;
 }
 
-fn get_beating_val(a: &str) -> &str {
-    return match a {
-        "A" => "B",
-        "B" => "C",
-        "C" => "A",
+fn get_losing_val(a: &char) -> &char {
+    match a {
+        'A' => &'C',
+        'B' => &'A',
+        'C' => &'B',
         _ => unreachable!(),
-    };
+    }
 }
 
-fn get_losing_val(a: &str) -> &str {
-    return match a {
-        "A" => "C",
-        "B" => "A",
-        "C" => "B",
-        _ => unreachable!(),
-    };
-}
-
-fn get_points_won(a: &str, b: &str) -> i128 {
+fn get_points_won(a: &char, b: &char) -> i128 {
     if b.eq(a) {
         return 3;
     }
-
-    if b.eq(get_beating_val(a)) {
+    if b.eq(get_winning_val(a)) {
         return 6;
     }
-
     if b.eq(get_losing_val(a)) {
         return 0;
     }
-
     unreachable!();
 }
 
 // 1 for Rock, 2 for Paper, and 3 for Scissors
-fn get_points_from_choice(b: &str) -> i128 {
+fn get_points_from_choice(b: &char) -> i128 {
     return match b {
-        "A" => 1,
-        "B" => 2,
-        "C" => 3,
+        'A' => 1,
+        'B' => 2,
+        'C' => 3,
         _ => unreachable!(),
     };
 }
 
 #[aoc(day2, part1)]
 pub fn part1(input: &[Round]) -> i128 {
-    let mut val = 0;
-
-    for v in input {
-        let b = match v.1.as_str() {
-            "X" => "A",
-            "Y" => "B",
-            "Z" => "C",
+    input.iter().fold(0, |mut val, v| {
+        let b = match v.1 {
+            'X' => 'A',
+            'Y' => 'B',
+            'Z' => 'C',
             _ => unreachable!(),
         };
-        let from_win = get_points_won(&v.0, b);
-        let from_choice = get_points_from_choice(b);
-        val += from_win + from_choice;
-    }
-
-    return val;
+        val += get_points_from_choice(&b) + get_points_won(&v.0, &b);
+        val
+    })
 }
 
 #[aoc(day2, part2)]
 pub fn part2(input: &[Round]) -> i128 {
-    let mut val = 0;
-
-    for v in input {
-        let b = match v.1.as_str() {
-            "X" => get_losing_val(&v.0),
-            "Y" => &v.0,
-            "Z" => get_beating_val(&v.0),
+    input.iter().fold(0, |mut val, v| {
+        let b = match v.1 {
+            'X' => get_losing_val(&v.0),
+            'Y' => &v.0,
+            'Z' => get_winning_val(&v.0),
             _ => unreachable!(),
         };
-        let from_choice = get_points_from_choice(b);
-        let from_win = get_points_won(&v.0, b);
-        let x = from_choice + from_win;
-        val += x;
-    }
-
-    return val;
+        val += get_points_from_choice(b) + get_points_won(&v.0, b);
+        val
+    })
 }
 
 #[cfg(test)]
